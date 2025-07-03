0d3a2715203c44f0b2713d744dbcea2f52949ea5
Jakub Dolejs <jakubdolejs@gmail.com>
2025-07-03 15:20:53 +0100

    Added publication

--- arcface/build.gradle.kts
+++ arcface/build.gradle.kts
@@ -2,7 +2,11 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
+    `maven-publish`
+    signing
 }
+
+version = "1.0.0"
 
 android {
     namespace = "com.appliedrec.verid3.facerecognition.arcface.cloud"
@@ -47,4 +51,56 @@ dependencies {
     androidTestImplementation(libs.verid.facedetection)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.appliedrec.verid3"
+            artifactId = "face-recognition-arcface-cloud"
+            afterEvaluate {
+                from(components["release"])
+            }
+            pom {
+                name.set("ArcFace face recognition for Ver-ID")
+                description.set("Face recognition implementation for Ver-ID SDK using ArcFace model")
+                url.set("https://github.com/AppliedRecognition/Face-Recognition-ArcFace-Android")
+                licenses {
+                    license {
+                        name.set("Commercial")
+                        url.set("https://raw.githubusercontent.com/AppliedRecognition/Face-Recognition-ArcFace-Android/main/LICENCE.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("appliedrec")
+                        name.set("Applied Recognition")
+                        email.set("support@appliedrecognition.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/AppliedRecognition/Face-Recognition-ArcFace-Android.git")
+                    developerConnection.set("scm:git:ssh://github.com/AppliedRecognition/Face-Recognition-ArcFace-Android.git")
+                    url.set("https://github.com/AppliedRecognition/Face-Recognition-ArcFace-Android")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/AppliedRecognition/Ver-ID-Releases-Android")
+            credentials {
+                username = project.findProperty("gpr.user") as String?
+                password = project.findProperty("gpr.token") as String?
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["release"])
 }
